43f3f215cc9599c9560b7d71ddd62bee0a8ccc09
sagarp197 <sagarp197@gmail.com.com>
2022-03-05 20:49:13 +0530

    Factors

---
+++ Factors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Factors
+    {
+        public static void DisplayPrimeFactor()
+        {
+            int a, b;
+            Console.WriteLine("Please enter your integer: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (b = 2; a > 1; b++)
+                if (a % b == 0)
+                {
+                    int x = 0;
+                    while (a % b == 0)
+                    {
+                        a /= b;
+                        x++;
+                    }
+                    Console.WriteLine($"{b} is a prime factor {x} times!");
+                }
+        }
+    }
+}
