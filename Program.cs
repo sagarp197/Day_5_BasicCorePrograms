8e7078dd84f8c74a5576e32a883044a42c674dcb
sagarp197 <sagarp197@gmail.com.com>
2022-03-05 19:59:13 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FlipCoinAndPercentage.DisplayPercentage();
+            //LeapYear.DisplayYearIsLeap();
+            //PowerOf2.PrintTableOfPower2();
+            HarmonicNumber.DisplayNthHarmonicNumber();
+
+        }
+    }
+}
